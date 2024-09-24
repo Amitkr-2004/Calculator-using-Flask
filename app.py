fb379e2f859c0d44206fa812b22ae9fdddae83a0
Amit Kumar <amitkumar990374@gmail.com>
2024-09-24 17:21:12 +0530

    Post Man request updated

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home_page():
 @app.route('/math',methods=['POST'])
 def math_ops():
     if(request.method=='POST'):
-        operation=request.form['operation']
+        operation=request.form['operation']  #from form class we are pulling information
         num1=int(request.form['num1'])
         num2=int(request.form['num2'])
 
@@ -32,6 +32,32 @@ def math_ops():
 
         return render_template("results.html",result=result)
 
+
+@app.route('/postman_request',methods=['POST'])
+def postman_math_ops():
+    if(request.method=='POST'):
+        operation=request.json['operation']
+        num1=int(request.json['num1'])
+        num2=int(request.json['num2'])
+
+        if(operation=='addition'):
+            r=num1+num2
+            result =" The sum of num1 and num2 is " +  str(r)
+
+        if(operation=='subtraction'):
+            r=num1-num2
+            result =  " The subtraction of num1 and num2 is " + str(r) 
+
+        if(operation=='multiplication'):
+            r=num1*num2
+            result =  "The multiplication of num1 and num2 is " + str(r) 
+
+        if(operation=='divide'):
+            r=num1/num2
+            result = "The division of num1 and num2 is" + str(r) 
+        
+
+        return jsonify(result)
 
 if __name__=='__main__':
     app.run(debug=True)
